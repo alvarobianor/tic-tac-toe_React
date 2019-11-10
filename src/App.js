73a3f22383ec8c4868c34af88a5035929943dec7
Álvaro Bianor <bianor.alvaro12@gmail.com>
2019-11-09 22:21:43 -0300

    adjusting the game#2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 
 //const alvaro = this.props.value;
 class Square extends React.Component {
@@ -7,7 +7,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => alert("click " + this.props.value)}
+        onClick={() => {
+          alert("Hey");
+        }}
       >
         {this.props.value}
       </button>
